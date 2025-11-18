d8b5d1fc7346ba912ebde7544300e8df6e921e6b
Denis Krupchenko <denis.krupchenko@hotcode.lv>
2025-11-18 19:32:45 +0400

    Updated eslint rules

--- apps/host/eslint.config.js
+++ apps/host/eslint.config.js
@@ -1,9 +1,11 @@
 import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -21,7 +23,15 @@ export default [
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'off' // TypeScript handles this
+    }
+  },
+  {
+    files: ['vite.config.ts'],
+    rules: {
+      'no-undef': 'off' // Node.js globals
     }
   }
 ];
